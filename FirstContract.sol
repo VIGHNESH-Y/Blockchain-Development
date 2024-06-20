baaaeb6b9f8024c21cced44dc6ae9a585d3c0e7b
Vighnesh Y <80388978+VIGHNESH-Y@users.noreply.github.com>
2024-06-20 21:28:38 +0530

    Update FirstContract.sol

--- FirstContract.sol
+++ FirstContract.sol
@@ -1,7 +1,8 @@
-pragma solidity >=0.4.16 <0.9.0
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
 contract SimpleStorage {
-    uint storedData;
+    uint public storedData;
 
     function set(uint x) public {
         storedData = x;
